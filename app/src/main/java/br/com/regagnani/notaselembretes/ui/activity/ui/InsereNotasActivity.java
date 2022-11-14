f766b784b0e157a848bae5d0f0a01559a9237b87
Luiz Fernando Regagnani <luiz.fernando.regagnani@gmail.com>
2022-11-13 22:00:57 -0300

    Recurso de inserir notas adicionado, ainda falta um recurso para remover notas

---
+++ app/src/main/java/br/com/regagnani/notaselembretes/ui/activity/ui/InsereNotasActivity.java
@@ -0,0 +1,14 @@
+package br.com.regagnani.notaselembretes.ui.activity.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InsereNotasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insere_notas);
+    }
+}
